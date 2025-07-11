57b5f1105da2fc23399d31fc028fd1e70f067b24
sakamata1 <laml69087@gmail.com>
2025-07-11 23:45:29 +0700

    Add function that provides info about coordinates on chess board

--- Chess.py
+++ Chess.py
@@ -30,11 +30,11 @@ def draw_board(surface):
 
 # Set up chess piece in board
 def draw_piece(surface, game=None):
-    for r in range(DIMENSION):
+    for r in range(DIMENSION - 1, -1, -1):
         for f in range(DIMENSION):
-            piece = game[f][r]
+            piece = game[r][f]
             if piece != "--":
-                surface.blit(IMAGES[piece], p.Rect(r * SQ_SIZE, f * SQ_SIZE, WIDTH, HEIGHT))
+                surface.blit(IMAGES[piece], p.Rect(f * SQ_SIZE, r * SQ_SIZE, WIDTH, HEIGHT))
 
 # Display board
 def draw_game(surface, game):
@@ -42,6 +42,14 @@ def draw_game(surface, game):
     load_images()
     draw_board(surface)
     draw_piece(surface, gs)
+
+# Display coordinates on chess board
+def chess_coordinates(ori_rank, ori_file):
+    rank = {1: "8", 2: "7", 3: "6", 4: "5", 5: "4", 6: "3", 7: "2", 8: "1"}
+    file = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h"}
+    r = rank[ori_rank]
+    f = file[ori_file]
+    print(f + r)
 
 # "Actually" display board
 def main():
@@ -54,6 +62,11 @@ def main():
         for event in p.event.get():
             if event.type == p.QUIT:
                 running = False
+            elif event.type == p.MOUSEBUTTONDOWN:
+                mouse_pos = p.mouse.get_pos()
+                r_coor = int(mouse_pos[1] // SQ_SIZE) + 1
+                f_coor = int(mouse_pos[0] // SQ_SIZE) + 1
+                chess_coordinates(r_coor, f_coor)
 
         screen.fill("white")
         game_state = ChessEngine.GameState()
